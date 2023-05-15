b5aa4604a4ff9dde5ce5d0f98cd4b19e8addd52d
Shreyas Mudakavi <shreyasmudak@gmail.com>
2023-05-15 09:24:48 +0530

    added installers model and routes

--- models/installersModel.js
+++ models/installersModel.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
 
-const installerSchema = new mongoose.Schema({
-  ID: {
-    type: String,
-    required: true,
+const installerSchema = new mongoose.Schema(
+  {
+    ID: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Please enter name."],
+    },
+    profilePic: {
+      type: String,
+    },
+    location: {
+      type: String,
+      required: [true, "Location is required."],
+    },
+    zip: {
+      type: String,
+      required: [true, "Please enter zip code."],
+    },
   },
-  name: {
-    type: String,
-    required: [true, "Please enter name."],
-  },
-  profilePic: {
-    type: String,
-  },
-  location: {
-    type: String,
-    required: [true, "Location is required."],
-  },
-  zip: {
-    type: String,
-    required: [true, "Please enter zip code."],
-  },
-});
+  { timestamps: true }
+);
 
 const installerModel = mongoose.model("installers", installerSchema);
 
