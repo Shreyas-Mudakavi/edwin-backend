4f995b6e63916d79c2b4f21bf919321bc44ccc91
Shreyas Mudakavi <shreyasmudak@gmail.com>
2023-08-04 16:50:09 +0530

    bug fixes

--- controllers/staticController.js
+++ controllers/staticController.js
@@ -5,7 +5,7 @@ const catchAsyncError = require("../utils/catchAsyncError");
 const ErrorHandler = require("../utils/errorHandler");
 
 exports.getStatic = catchAsyncError(async (req, res, next) => {
-  const staticContent = await staticModel.find();
+  const staticContent = await staticModel.findOne();
 
   if (!staticContent) {
     return next(new ErrorHandler("Content will be added soon!", 404));
